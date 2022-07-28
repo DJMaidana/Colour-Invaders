6ce79ecab16908a9f2da416c3eaa0b95fb0e76d1
Damian Maidana <damian.j.maidana@gmail.com>
2022-07-28 10:57:06 -0300

    Added enemy attacks

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,28 +6,77 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] int hitPoints = 1;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject enemyBullet;
 
-    // Update is called once per frame
+    public bool isInFront;
+    bool hasFired;
+    float firingRate;
+
     void Update()
     {
-        
+        if (isInFront)
+        {
+            if (!hasFired)
+            {
+                firingRate = Random.Range(5,10);
+                Invoke("Shoot", firingRate);
+                hasFired = true;                //  locks firing ability until the method Shoot() reloads it
+            }
+
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 bulletPosition = transform.position + Vector3.down;           // From enemy position get offset for the bullet position
+        Quaternion bulletRotation = Quaternion.Euler(0f, 0f, -180f);          // Rotate Bullet so it goes down
+        Instantiate(enemyBullet, bulletPosition, bulletRotation);       
+        hasFired = false;                                                     // Enables firing
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-            if (hitPoints > 1)
-            {
-                hitPoints--;
-            }
-            else
-            {
-                EnemyDeath();
-            }
+        switch (other.gameObject.tag)   
+        {
+            case "Player_Bullet":       //  Sequence for being shot by the player
+
+                HitByPlayer();
+                break;
+
+            case "Cover":               //  Sequence for crashing against Covers
+
+                HitSelf();
+                break;
+
+        
+        }
+    }
+
+    void HitByPlayer()
+    {   
+        if (hitPoints > 1)
+        {
+            //Add score on hit
+            hitPoints--;
+        }
+        else
+        {
+            //Calculate neighbors
+            //Calculate score on death
+            EnemyDeath();
+        }
+    }
+
+    void HitSelf()
+    {
+        if (hitPoints > 1)
+        {
+            hitPoints--;
+        }
+        else
+        {
+            EnemyDeath();
+        }
     }
 
     void EnemyDeath()
