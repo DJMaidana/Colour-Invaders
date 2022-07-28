e2b36049daf6c992139cc3aa3658ee557456544a
Damian Maidana <damian.j.maidana@gmail.com>
2022-07-28 11:07:23 -0300

    Added player health

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 1f;
+    [SerializeField] int hitPoints = 3;
 
     [SerializeField] GameObject playerBullet;
 
@@ -33,5 +34,43 @@ public class PlayerController : MonoBehaviour
         Instantiate(playerBullet, bulletPosition, transform.rotation);      // Instantiate bullet
 
         bulletFired = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (other.gameObject.tag)   
+        {
+            case "Enemy_Bullet":       //  Sequence for being shot by an enemy
+
+                HitByEnemy();
+                break;
+
+            case "Enemy":               //  Sequence for crashing against enemies
+
+                GameOver();
+                break;
+        }
+    }
+
+    void HitByEnemy()
+    {
+        if (hitPoints > 1)
+        {
+            PlayerDamaged();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    void PlayerDamaged()
+    {
+        hitPoints--;
+    }
+
+    void GameOver()
+    {
+        Destroy(gameObject);
     }
 }
